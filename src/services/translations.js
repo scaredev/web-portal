f2fc2638aa77a0b9844a86257bba1c806e561a4d
Adones Pitogo <pitogo.adones@gmail.com>
2018-10-06 15:56:29 +0800

    added translations.js

---
+++ src/services/translations.js
@@ -0,0 +1,35 @@
+
+const Translations = {
+  en: {
+    buttons: {
+      INSERT_COIN: 'Insert Coin',
+      CONNECT: 'Connect',
+      BUY_VOUCHER: 'Buy Voucher',
+      VIEW_RATES: 'View Rates',
+      CLOSE: 'Close',
+      USE_VOUCHER: 'Use Voucher',
+      IM_DONE_PAYING: "I'm Done Paying",
+      PLEASE_WAIT: 'Please wait',
+      PAUSE_TIME: 'Pause Time'
+    },
+    PACKAGE_RATES: 'Package Rates',
+    DO_YOU_HAVE_A_VOUCHER: 'Do you have a voucher?',
+    REMAINING_TIME: 'Remaining Time',
+    AMOUNT: 'Amount',
+    TIME: 'Time',
+    INSERT_COIN_NOW: 'Insert Coin Now',
+    WAITING_FOR_PAYMENT: 'Waiting for payment',
+    status: {
+      STATUS: 'Status',
+      CONNECTED: 'Connected',
+      DISCONNECTED: 'Disconnected',
+      PAUSED: 'Paused'
+    },
+    toast: {
+      error: {},
+      success: {}
+    }
+  }
+}
+
+export default Translations
