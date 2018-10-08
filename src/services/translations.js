97dbb304c9c9a34366196eeb8dea642b23a90882
Adones Pitogo <pitogo.adones@gmail.com>
2018-10-08 17:13:51 +0800

    finish translation

--- src/services/translations.js
+++ src/services/translations.js
@@ -55,52 +55,51 @@ const Translations = {
   // Spanish
   es: {
     buttons: {
-      INSERT_COIN: 'Inserte Moneda',
+      INSERT_COIN: 'Inserte Una Moneda',
       CONNECT: 'Conectar',
-      BUY_VOUCHER: 'Comprar Cupón',
+      BUY_VOUCHER: 'Comprar Voucher',
       VIEW_RATES: 'Ver Tarifas',
       CLOSE: 'Cerca',
       CANCEL: 'Cancelar',
-      USE_VOUCHER: 'Use Voucher',
-      IM_DONE_PAYING: `I'm Done Paying`,
-      DONE_PAYING_FOR_VOUCHER: 'Done paying for voucher',
-      PAUSE_TIME: 'Pause Time',
-      START_INTERNET: 'Start Internet',
-      COPY_TO_CLIPBOARD: 'Copy To Clipboard'
+      USE_VOUCHER: 'Usar Voucher',
+      IM_DONE_PAYING: `He terminado de pagar`,
+      DONE_PAYING_FOR_VOUCHER: 'Hecho pagar por el voucher',
+      PAUSE_TIME: 'Tiempo de pausa',
+      START_INTERNET: 'Iniciar Internet',
+      COPY_TO_CLIPBOARD: 'Copiar al Portapapeles'
     },
-    CODE: 'Code',
-    WARNING: 'Warning',
+    CODE: 'Codigo',
+    WARNING: 'Advertencia',
     VOUCHER: 'Voucher',
     PLEASE_WAIT: 'Por favor espera',
-    PACKAGE_RATES: 'Package Rates',
-    DO_YOU_HAVE_A_VOUCHER: 'Do you have a voucher?',
-    REMAINING_TIME: 'Remaining Time',
-    AMOUNT: 'Amount',
-    TIME: 'Time',
+    PACKAGE_RATES: 'Tarifas de Paquete',
+    DO_YOU_HAVE_A_VOUCHER: 'Tienes un Voucher?',
+    REMAINING_TIME: 'Tiempo Restante',
+    AMOUNT: 'Cantidad',
+    TIME: 'Hora',
     INSERT_COIN_NOW: 'Inserte Moneda Ahora',
-    PAY_FOR_VOUCHER: 'Pay For Voucher',
-    WAITING_FOR_PAYMENT: 'Waiting for payment',
+    PAY_FOR_VOUCHER: 'Pagar por el Voucher',
+    WAITING_FOR_PAYMENT: 'A la espera del pago',
     YOUR_TIME: 'Su Tiempo',
-    HERES_YOUR_VOUCHER: "Aquí Está Tu Cupón",
-    PRINT_SCREEN: 'Please print screen this page for backup.',
+    HERES_YOUR_VOUCHER: "Aquí Está Tu Voucher",
+    PRINT_SCREEN: 'Por favor imprima la pantalla de esta página para copia de seguridad.',
     status: {
-      STATUS: 'Status',
-      CONNECTED: 'Connected',
-      DISCONNECTED: 'Disconnected',
-      RECONNECT: 'Pls. Reconnect',
-      PAUSED: 'Paused'
+      STATUS: 'Estado',
+      CONNECTED: 'Conectado',
+      DISCONNECTED: 'Desconectado',
+      RECONNECT: 'Pls. Reconecte',
+      PAUSED: 'En Pausa'
     },
     machine_state: {
-      UNABLE_TO_SYNCHRONIZE: 'Unable to synchronize connection status',
-      REBOOTING: 'The machine is rebooting',
-      SHUTTING_DOWN: 'The machine is shutting down',
-      WIFI_RESTARTING: 'The wifi is restarting'
+      UNABLE_TO_SYNCHRONIZE: 'No se puede sincronizar el estado de la conexión',
+      REBOOTING: 'La máquina se está reiniciando',
+      SHUTTING_DOWN: 'La máquina se está apagando',
+      WIFI_RESTARTING: 'El wifi se está reiniciando'
     },
     toast: {
       error: {
-        MACHINE_IS_OFFLINE: 'Machine is OFFLINE'
-      },
-      success: {}
+        MACHINE_IS_OFFLINE: 'La máquina está fuera de línea'
+      }
     }
   }
   // end spanish
